47186e200808493e19cc0630c15604adf05199f6
roborant <singh_k_anil@hotmail.com>
2014-11-17 23:31:27 +1100

    set up EntityState = System.Data.Entity.EntityState to use correct entity state

--- JewelOfIndiaBuilder/Controllers/LocationController.cs
+++ JewelOfIndiaBuilder/Controllers/LocationController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using JewelOfIndiaBuilder.Models;
+using EntityState = System.Data.Entity.EntityState;
 
 namespace JewelOfIndiaBuilder.Controllers
 {
